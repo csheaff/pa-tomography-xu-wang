72dfacb825df34f6a153b78c273d0da34334e592
Clay Sheaff <claysheaff@protonmail.com>
2020-05-25 20:18:42 -0700

    something working without hannwin but slowww

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -43,8 +43,14 @@ fn complex2real() {
 }
 
 
-fn get_signals(tar_info: &ArrayView1<f64>, xd:  &Array1<f64>, t: &Array1<f64>, z_targ: f64) -> Array3<f64> {
+fn step_fn(x: &Array1<f64>) -> Array1<f64> {
+  let y = 0.5 * (x.mapv(f64::signum) + 1.0);
+  y 
+}
 
+
+fn get_signals(tar_info: &ArrayView1<f64>, xd:  &Array1<f64>, t: &Array1<f64>, z_targ: f64) -> Array3<f64> {
+    // Clay this is all very slow and I don't know why.
     let yd = xd.clone();
     let det_len = 2e-3;
     let n_subdet = 25;
@@ -55,24 +61,27 @@ fn get_signals(tar_info: &ArrayView1<f64>, xd:  &Array1<f64>, t: &Array1<f64>, z
 
     let fs = 1.0 / (t[1] - t[0]);
     let fc = 4e6;
-    let c = 1484;
+    let c = 1484.0;
     
     let n_det_x = xd.len();
     let n_det_y = yd.len();
     let mut sigs = Array3::<f64>::zeros((t.len(), n_det_x, n_det_x));
     for xi in 0..n_det_x {
+	println!("{ }", xi);
 	for yi in 0..n_det_y {
 	    let mut pa_sig = Array1::<f64>::zeros(t.len());
 	    for m in 0..n_subdet {
 		for n in 0..n_subdet {
-		    let det_xyz = array![xd[xi] + subdet_offset[m], yd[yi] + subdet_offset[n], 0.0];
+ 		    let det_xyz = array![xd[xi] + subdet_offset[m], yd[yi] + subdet_offset[n], 0.0];
 		    let tar_xyz = tar_info.slice(s![..3]);
-		    let R = norm::Norm::norm_l2(&(det_xyz - tar_xyz));
-		    let tar_rad = &tar_info[3];
-		    
+		    let r = norm::Norm::norm_l2(&(det_xyz - tar_xyz));
+		    let tar_rad = tar_info[3];
+		    let step_fn_arg = tar_rad - 1.0* (r - c * t).mapv(f64::abs);
+		    let step_fn_arg = t.mapv(f64::abs);  // why is mapv so slow??
+		    pa_sig = pa_sig + step_fn(&step_fn_arg) * (r - c * t) / (2.0 * r);
 		}
 	    }
-	    let mut pr = pa_sig / n_subdet as f64;
+	    let pr = pa_sig / n_subdet as f64;
 	    let mut slice = sigs.slice_mut(s![.., xi, yi]);
 	    slice.assign(&pr);
 	}
@@ -91,7 +100,7 @@ fn main() {
     //    complex2real()
 
     let z_targ = 15.0;
-    let tar_info: Array2<f64> = 1e-3 * array![[18.0, 0.0, z_targ, 1.5],[-18.0, 0.0, z_targ, 1.5],[9.0, 0.0, z_targ, 1.5],[-9.0, 0.0, z_targ, 1.5],[0.0, 0.0, z_targ, 1.5],[0.0, 12.0, z_targ, 4.0],[0.0, -12.0, z_targ, 4.0]];
+    let tar_info: Array2<f64> = 1e-3 * array![[18.0, 0.0, z_targ, 1.5]]; //,[-18.0, 0.0, z_targ, 1.5],[9.0, 0.0, z_targ, 1.5],[-9.0, 0.0, z_targ, 1.5],[0.0, 0.0, z_targ, 1.5],[0.0, 12.0, z_targ, 4.0],[0.0, -12.0, z_targ, 4.0]];
     let n_targ = tar_info.shape()[0];
     
     let aper_len = 60e-3;
@@ -113,3 +122,17 @@ fn main() {
     }
 //    println!("{:?}", xd)
 }
+
+
+
+// fn main2() {
+
+// //    let x = array![-1.0, -0.5, -0.25, 0.0, 0.25, 0.5, 1.0];
+// //    let x = Array::range(0.0, 1300.0, 1.0);
+//     let x = [1.0,2.0,3.0];
+//     let y = [5.0,6.0,7.0];
+//     let z = x - y;
+
+//     println!("{:?}", z);
+// //    println!("{:?}", y);
+// }
